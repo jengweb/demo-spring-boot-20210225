1fd3f9f5aba182373d28467c4fd3d34f4f9aaab1
Jengweb <jengweb@hotmail.com>
2021-02-26 15:41:30 +0700

    [Updated]: Add ApplicationContext

--- users/src/main/java/com/example/users/UsersApplication.java
+++ users/src/main/java/com/example/users/UsersApplication.java
@@ -1,13 +1,29 @@
 package com.example.users;
 
+import com.example.users.users.TableUser;
+import com.example.users.users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class UsersApplication {
 
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@PostConstruct
+//	public void initialData() {
+//		TableUser tableUser = new TableUser(1, "Jengweb", "jengweb@gmail.com");
+//		userRepository.save(tableUser);
+//	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(UsersApplication.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(UsersApplication.class, args);
 	}
 
 }
